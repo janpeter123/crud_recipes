e48b5fd8f4b53c784d67a7b45a19a36c0700766a
janpeter123 <janpetermr@gmail.com>
2022-03-09 15:40:13 -0300

    categories added

--- front-end/src/components/categories.js
+++ front-end/src/components/categories.js
@@ -1,9 +1,59 @@
 import "../styles/components/categories.css";
+import React from "react";
+import Container from "@mui/material/Container";
+import KeyboardArrowLeftIcon from "@mui/icons-material/KeyboardArrowLeft";
+import KeyboardArrowRightIcon from "@mui/icons-material/KeyboardArrowRight";
+import { IconButton } from "@mui/material/";
 
 function Categories() {
+  //deve ter 6 categorias.
+  const list = [1, 2, 3, 4, 5, 6];
+  const [index, setIndex] = React.useState(1);
+
   return (
-    <section className="categories-section">
-      <h1>teste</h1>
+    <section className="teste">
+      <IconButton
+        className="scroll-button"
+        onClick={() => {
+          if (index > 1) {
+            setIndex(index - 1);
+          }
+        }}
+      >
+        <a href={`#${index}`}>
+          <KeyboardArrowLeftIcon />
+        </a>
+      </IconButton>
+      <section className="categories-section">
+        {list.map((element) => {
+          return (
+            <Container
+              className="catecories-card"
+              id={element}
+              key={element}
+              sx={{
+                background: `linear-gradient(0deg, rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)),
+            url(/assets/pizza.png)`,
+                backgroundSize: "cover",
+              }}
+            >
+              <h1>{`teste ${element}`}</h1>
+            </Container>
+          );
+        })}
+      </section>
+      <IconButton
+        className="scroll-button"
+        onClick={() => {
+          if (index < list.length) {
+            setIndex(index + 1);
+          }
+        }}
+      >
+        <a href={`#${index}`}>
+          <KeyboardArrowRightIcon />
+        </a>
+      </IconButton>
     </section>
   );
 }
