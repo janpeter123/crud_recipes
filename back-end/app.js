fbe429f9b944b957f155a39935bf0fd37d00eff7
janpeter123 <janpetermr@gmail.com>
2022-03-14 16:46:54 -0300

    comments added

--- back-end/app.js
+++ back-end/app.js
@@ -3,32 +3,42 @@ var express = require("express");
 
 //# Mongoose
 const mongoose = require("mongoose");
+const recipe_schema = require("./models/recipes");
 
-// #DB Credentials
+// #DB Credentials .env file
 const dotenv = require("dotenv").config();
 const credentials = dotenv.parsed;
 
+// # Configuring credentials and mongodb schema
 const Schema = mongoose.Schema;
 const ObjectId = Schema.ObjectId;
-const recipe_schema = require("./models/recipes");
 const uri = `mongodb+srv://${credentials.MONGO_DB_ROLE}:${credentials.MONGO_DB_PASSWORD}@cluster0.83pjp.mongodb.net/crud?retryWrites=true&w=majority`;
 
-mongoose.connect(uri, (err, docs) => {
-  //Using a specific collection
+//Connecting to the DB
 
-  if (err) {
-  } else {
-    var app = express();
+try {
+  mongoose.connect(uri, (err, docs) => {
+    if (err) {
+    } else {
+      var app = express();
 
-    const Recipes = mongoose.model("recipe", recipe_schema);
+      //getting recipes
+      const Recipes = mongoose.model("recipe", recipe_schema);
 
-    app.get("/get_recipes", async function (req, res) {
-      // res.setHeader("Content-Type", "application/json");
-      Recipes.find({}, { _id: ObjectId, recipe_name: 1, review:1 }, function (err, docs) {
-        res.send(docs);
+      app.get("/get_recipes", async function (req, res) {
+        // res.setHeader("Content-Type", "application/json");
+        Recipes.find(
+          {},
+          { _id: ObjectId, recipe_name: 1, review: 1 },
+          function (err, docs) {
+            res.send(docs);
+          }
+        );
       });
-    });
-    console.log("Listening on port 5001");
-    app.listen(5001);
-  }
-});
+      console.log("Listening on port 5001");
+      app.listen(5001);
+    }
+  });
+} catch (e) {
+  console.log(e);
+}
