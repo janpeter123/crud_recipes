3b5e7f13f110f694c34a03fce1be8cb5c5200505
janpeter123 <janpetermr@gmail.com>
2022-04-04 16:24:19 -0300

    commented on code

--- back-end/app.js
+++ back-end/app.js
@@ -1,8 +1,12 @@
 //# Express Dependencies
 var express = require("express");
+
+//Passport JS
 const passport = require("passport");
-const LocalStrategy = require("passport-local").Strategy;
+//Express Session
 const session = require("express-session");
+
+//Controllers
 const { catalog_controller } = require("./controllers/catalog_controller");
 const { recipe_controller } = require("./controllers/recipe_controller");
 const {
@@ -19,9 +23,9 @@ const {
   update_recipe_review_controller,
 } = require("./controllers/update_recipe_review_controller");
 const { login_controller } = require("./controllers/login_controller");
-//# Mongoose
-const mongoose = require("mongoose");
 
+// Mongoose
+const mongoose = require("mongoose");
 
 // #DB Credentials .env file
 const dotenv = require("dotenv").config();
@@ -33,12 +37,15 @@ const uri = `mongodb+srv://${credentials.MONGO_DB_ROLE}:${credentials.MONGO_DB_P
 
 //Connecting to the DB
 try {
+  //Trying to connect to the mongo DB
   mongoose.connect(uri, (err, docs) => {
     if (err) {
     } else {
       var app = express();
+      //Using JSON on express
       app.use(express.json());
       app.use(express.urlencoded({ extended: true }));
+      //Using session
       app.use(
         session({
           secret: "secretcode",
@@ -46,23 +53,27 @@ try {
           saveUninitialized: true,
         })
       );
-
+      //Initializing passport and session
       app.use(passport.initialize());
       app.use(passport.session());
+      //Using passportConfig file on passport
       require("./passportConfig")(passport);
 
+      //Get Routes
       app.get("/get_catalog", catalog_controller);
       app.get("/get_recipe", recipe_controller);
       app.get("/get_categories", categories_controller);
       app.get("/get_measurement_units", measurement_controller);
       app.get("/get_countries", countries_controller);
       
+      //Patch routes
       app.patch("/update_recipe", update_recipe_review_controller);
 
+      //Post Routes
       app.post("/create_recipe", create_recipe_controller);
       app.post("/login", login_controller);
 
-
+      //Starting the server
       console.log("Listening on port 5001");
       app.listen(5001);
     }
